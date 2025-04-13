3f46ad6867253595cacc9417451263a2f7155d99
shahreaz0 <ashahreaz@gmail.com>
2025-04-14 00:09:32 +0600

    add extra option to query

--- app/task/task-list.tsx
+++ app/task/task-list.tsx
@@ -2,40 +2,58 @@
 
 import { useGetTasks } from "@/gen/hooks/tasks/use-get-tasks";
 import { GetTasksQueryParamsDoneEnum } from "@/gen/types/tasks/get-tasks-type";
+import { keepPreviousData } from "@tanstack/react-query";
 import { useState } from "react";
 
 export function TaskList() {
-  const [done, setDone] = useState("true");
-  const { data: tasks } = useGetTasks({ done: done as GetTasksQueryParamsDoneEnum });
+  const [done, setDone] = useState<string | undefined>(undefined);
+  const { data: tasks, isLoading } = useGetTasks(
+    {
+      done: done as GetTasksQueryParamsDoneEnum,
+    },
+    { query: { placeholderData: keepPreviousData } }
+  );
 
   return (
     <section>
       <select
         value={done}
         onChange={(e) => {
+          if (e.target.value === "undefined") {
+            setDone(undefined);
+            return;
+          }
+
           setDone(e.target.value);
         }}
         className="border mb-10"
       >
+        <option value="undefined">All</option>
         <option value="true">Complete</option>
         <option value="false">Pending</option>
       </select>
 
-      <ul className="list-disc ">
-        {!tasks?.data.length ? (
-          <p>no tasks</p>
-        ) : (
-          <>
-            {tasks?.data.map((task) => (
-              <li key={task.id}>
-                <span>
-                  {task.name} <span className="ml-2">{task.done ? "✔️" : "❌"}</span>
-                </span>
-              </li>
-            ))}
-          </>
-        )}
-      </ul>
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          <ul className="list-disc ">
+            {!tasks?.data.length ? (
+              <p>no tasks</p>
+            ) : (
+              <>
+                {tasks?.data.map((task) => (
+                  <li key={task.id}>
+                    <span>
+                      {task.name} <span className="ml-2">{task.done ? "✔️" : "❌"}</span>
+                    </span>
+                  </li>
+                ))}
+              </>
+            )}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
